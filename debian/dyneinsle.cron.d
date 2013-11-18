7bed83a480a2603c1f813ed3f208463b27b328cb
Robert Einsle <robert@einsle.de>
2013-11-18 18:12:24 +0100

    reducing update-time to 5 mins, because only updated while changed
    Signed-off-by: Robert Einsle <robert@einsle.de>

--- debian/dyneinsle.cron.d
+++ debian/dyneinsle.cron.d
@@ -2,4 +2,4 @@
 # Regular cron jobs for the dyneinsle package
 #
 @reboot         root	[ -x /usr/bin/update-dyneinsle.sh ] && /usr/bin/update-dyneinsle.sh
-*/15  * * * *	root	[ -x /usr/bin/update-dyneinsle.sh ] && /usr/bin/update-dyneinsle.sh
+*/5   * * * *	root	[ -x /usr/bin/update-dyneinsle.sh ] && /usr/bin/update-dyneinsle.sh

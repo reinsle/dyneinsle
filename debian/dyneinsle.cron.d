4b8813e964d5d9fa858505045596dc36250d4908
Robert Einsle <robert@einsle.de>
2013-11-18 11:31:15 +0100

    fix crontab-entries
    Signed-off-by: Robert Einsle <robert@einsle.de>

--- debian/dyneinsle.cron.d
+++ debian/dyneinsle.cron.d
@@ -1,4 +1,5 @@
 #
 # Regular cron jobs for the dyneinsle package
 #
-0/15  * * * *	root	[ -x /usr/bin/update-dyneinsle.sh ] && /usr/bin/update-dyneinsle.sh
+@reboot         root	[ -x /usr/bin/update-dyneinsle.sh ] && /usr/bin/update-dyneinsle.sh
+*/15  * * * *	root	[ -x /usr/bin/update-dyneinsle.sh ] && /usr/bin/update-dyneinsle.sh
